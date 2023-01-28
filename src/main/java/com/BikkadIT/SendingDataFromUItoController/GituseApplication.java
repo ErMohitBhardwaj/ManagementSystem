e3a23011ac55b5baa64dd49d0c3bc7929f71f410
ermohitsharma <mohitsharma127308@gmail.com>
2023-01-28 20:42:17 +0530

    changes by Mohit

---
+++ src/main/java/com/BikkadIT/SendingDataFromUItoController/GituseApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SendingDataFromUItoController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GituseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GituseApplication.class, args);
+	}
+
+}
